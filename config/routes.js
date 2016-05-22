58b1f4f2a7137837099c6d5a36ec419e792a5ecd
Carlos Allende <carlosallendesanz@gmail.com>
2016-05-21 18:35:55 -0700

    angular up and running

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,14 @@
 var express = require ('express');
 var router = express.Router();
 var usersController = require('../controllers/users');
+
+/* GET home page */
+router.get('/', function (req, res, next) {
+  res.sendfile('frontend/index.html');
+});
+
+
+/* API Routes */
 router.route('/api/users')
   .get(usersController.index)
   .post(usersController.create)
---
+++ frontend/js/users/usersControllers.js
@@ -0,0 +1,12 @@
+(function() {
+  angular.module('solarEclipsePlanner')
+  .controller('usersController', usersController)
+
+  usersController.$inject = [];
+
+  function usersController() {
+    var vm=this
+    vm.test="testing"
+
+  };
+})();
