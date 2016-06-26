d203837f50438b5b2c095b433b72b577503643f2
Carlos Allende <carlosallendesanz@gmail.com>
2016-06-25 17:39:10 -0700

    adding guest mode

--- frontend/js/auth/navbar.controller.js
+++ frontend/js/auth/navbar.controller.js
@@ -5,14 +5,21 @@
     .module("solarEclipsePlanner")
     .controller("NavbarController", NavbarController);
 
-  NavbarController.$inject = ["$log", "authService"];
+  NavbarController.$inject = ["$log", "authService", "$scope", "$location"];
 
-  function NavbarController($log, authService) {
+  function NavbarController($log, authService, $scope, $location) {
     var vm = this;
 
     vm.authService = authService;
+    vm.currentPath = currentPath;
 
     $log.info("NavbarController loaded!");
+
+    function currentPath () {
+      $scope.currentPath = $location.path();
+      return $scope.currentPath
+    };
+
 
     $(document).ready(function(){
       // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
